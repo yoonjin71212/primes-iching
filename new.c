6a00208142ca60605198641f652ff49f7d5b0167
Yunjin Lee <yoonjin67@gmail.com>
2024-04-07 12:04:37 +0900

    Update new.c

--- new.c
+++ new.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
 #include <stdatomic.h>
+#include <math.h>
 
-// 소수 판별 함수
-atomic_int isPrime(atomic_int n) {
-    if (n <= 1) return isPrime(--n); // 1 이하의 수는 소수가 아님
-    printf("%x ",n);
-    if (n <= 3) return isPrime(n++); // 2와 3은 소수임
-    if (n % 2 == 0 || n % 3 == 0) return isPrime(--n); // 2나 3으로 나누어 떨어지면 소수가 아님
-    for (atomic_int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return isPrime(--n);
+typedef atomic_int aint;
+FILE *f;
+struct timespec *req,*rem;
+void pattern(aint x, aint y)
+{
+    if (x<y)
+    {
+	req++;
+	rem--;
+	
+        fprintf(f,"%c%c",x,y);
+    } else if (x>y) {
+	rem++;
+	req--;
+        fprintf(f,"%c%c",y,x);
+    } else if(x==y) {
+	rem--;
+	req++;
+        fprintf(f,"%c%c",y,x);
+        fprintf(f,"%c%c",x,y);
     }
-    return isPrime(n++);
 }
 
-void main(int argc, char **argv) {
-    // 여기에 홀수 감소시키기 및 짝수 증가시키기 로직 구현
-    while (1) {
-        isPrime((atomic_int)0);
-    }
+void main(aint argc, char **argv)
+{
+  while(1) {
+    f=fopen("./pattern", "r");
+    pattern(argc,argc);
+    fclose(f);
+  }
+  return 0;
 }
-
